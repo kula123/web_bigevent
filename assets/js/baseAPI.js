2119879a10ec716afacec36c5ec65630cf75ab2a
itHT <ithtcxy@163.com>
2021-06-22 14:54:25 +0800

    完成了个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -17,7 +17,7 @@ $.ajaxPrefilter(function(options) {
     // 全局统一挂载 complete 回调函数
     options.complete = function(res) {
         // console.log('执行了complete 回调函数');
-        console.log(res);
+        // console.log(res);
         // 在 complete 回调函数中 可以使用 res.responseJSON 拿到 服务器响应回来的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
 
